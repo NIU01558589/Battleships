8a21a4526b8e2089ad046feb4468880f35d8e781
NIU01558589 <1558589@uab.cat>
2024-12-01 17:54:22 +0100

    Path coverage place ships

--- src/test/java/org/uab/joclau/battleships/controller/GameControllerTest.java
+++ src/test/java/org/uab/joclau/battleships/controller/GameControllerTest.java
@@ -38,6 +38,31 @@ class GameControllerTest {
         Ship posicioInferiorDreta = new Ship(BoardUtils.generateCells(9,7,3,true),3);
         assertTrue(player1.placeShips(posicioInferiorDreta,9,7,true));
     }
+    // PATH COVERAGE
+    @Test
+    void testPathCoveragePlaceShips() {
+        GameController gameController = new GameController("Player1", "Player2");
+        Player player1 = gameController.getPlayer1();
+
+        // Cami 1: Colocacio exitosa
+        Ship pathSuccess =new Ship(BoardUtils.generateCells(0, 0, 2, true), 2);
+        assertTrue(player1.placeShips(pathSuccess, 0, 0, true));
+
+        // Cami 2: Coordenades fora de limit
+        Ship pathErrorPosition = new Ship(BoardUtils.generateCells(10, 10, 2, true), 2);
+        assertFalse(player1.placeShips(pathErrorPosition, 10, 10, true));
+
+        // Cami 3: Solapament amb altre vaixell
+        Ship pathAux = new Ship(BoardUtils.generateCells(0, 0, 2, true), 2);
+        Ship pathOccupiedPosition = new Ship(BoardUtils.generateCells(0, 1, 3, true), 3);
+        player1.placeShips(pathAux,0,0,true);
+        assertFalse(player1.placeShips(pathOccupiedPosition, 1, 10, true));
+
+        //Cami 4: Dimensions del vaixell surten dels limits
+        Ship dimensionsForaLimit =new Ship(BoardUtils.generateCells(0, 0, 2, true), 2);
+        assertTrue(player1.placeShips(dimensionsForaLimit, 0, 0, true));
+    }
+    // LOOP ANIUAT
     @Test
     void playGame() {
     }
